519597fdc86340882501583a1f7be5cc343cc6e6
jhonatan Padilla <jhoalparo1991@gmail.com>
2022-12-10 13:27:50 -0500

    se fusiono el detalle de productos, adicionalmente se agrego interactividad al hacer click en los productos

--- data.js
+++ data.js
@@ -1,59 +1,82 @@
+
 const data = [
 
     {
+        id: 1,
         name: 'Bike',
         price: 300,
-        image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 2,
         name: 'MotorBike',
         price: 1900,
-        image: 'https://images.pexels.com/photos/2393816/pexels-photo-2393816.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/2393816/pexels-photo-2393816.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 3,
         name: 'Shoes',
         price: 54,
-        image: 'https://images.pexels.com/photos/2562992/pexels-photo-2562992.png?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/2562992/pexels-photo-2562992.png?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 4,
         name: 'Casual Shoes',
         price: 30,
-        image: 'https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 5,
         name: 'Shoes sport',
         price: 120,
-        image: 'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 6,
         name: 'Shoes women',
         price: 98,
-        image: 'https://images.pexels.com/photos/336372/pexels-photo-336372.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/336372/pexels-photo-336372.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 7,
         name: 'Dress red',
         price: 205,
-        image: 'https://images.pexels.com/photos/1755428/pexels-photo-1755428.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/1755428/pexels-photo-1755428.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 8,
         name: 'Dress mini pink',
         price: 205,
-        image: 'https://images.pexels.com/photos/2235071/pexels-photo-2235071.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/2235071/pexels-photo-2235071.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 9,
         name: 'Nokia cellphone',
         price: 450,
-        image: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 10,
         name: 'Mac Desktop',
         price: 1560,
-        image: 'https://images.pexels.com/photos/5082554/pexels-photo-5082554.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/5082554/pexels-photo-5082554.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
     {
+        id: 11,
         name: 'Mac book pro',
         price: 2012,
-        image: 'https://images.pexels.com/photos/3153204/pexels-photo-3153204.jpeg?auto=compress&cs=tinysrgb&w=400'
+        image: 'https://images.pexels.com/photos/3153204/pexels-photo-3153204.jpeg?auto=compress&cs=tinysrgb&w=400',
+        description: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero beatae repellendus qui rem. Fugit vitae itaque quibusdam! Ab animi, omnis'
     },
 
 
--- main.js
+++ main.js
@@ -12,16 +12,28 @@ const cardsContainer = document.querySelector('.cards-container')
 const shoppingCart = document.querySelector('.shopping-cart')
 
 const closeWithArrow = document.querySelector('.flechita')
+const closeProductDetail = document.querySelector('.product-detail .close')
+const productDetail = document.querySelector('.product-detail')
+
+const productDetailImage = document.querySelector('.product-image');
+const productDetailPrice = document.querySelector('.product-info-price');
+const productDetailName = document.querySelector('.product-info-name');
 
 toggleMenu.addEventListener('click', toggleMenuDesktop);
 menuBarIcon.addEventListener('click', toggleMenuBar);
 menuShoppingCart.addEventListener('click', toggleShoppingCart);
 
 closeWithArrow.addEventListener('click', toggleShoppingCart)
+closeProductDetail.addEventListener('click', closeDetailProducts)
 
 function toggleMenuDesktop() {
 
     let isCart = cart.classList.contains('inactive');
+    let isProductDetail = productDetail.classList.contains('inactive')
+
+    if (!isProductDetail) {
+        productDetail.classList.add('inactive')
+    }
 
     if (!isCart) {
         cart.classList.add('inactive')
@@ -32,20 +44,37 @@ function toggleMenuDesktop() {
 
 function toggleMenuBar() {
     let isShoppingCart = cart.classList.contains('inactive')
+
+    let isProductDetail = productDetail.classList.contains('inactive')
+
+    if (!isProductDetail) {
+        productDetail.classList.add('inactive')
+    }
+
     if (!isShoppingCart) {
         cart.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
 }
 
+function closeDetailProducts() {
+    productDetail.classList.add('inactive')
+}
 function toggleShoppingCart() {
 
     let isMenuMobile = mobileMenu.classList.contains('inactive')
     let isNavbarMenu = navbarMenu.classList.contains('inactive')
+    let isProductDetail = productDetail.classList.contains('inactive')
+
+    if (!isProductDetail) {
+        productDetail.classList.add('inactive')
+    }
+
 
     if (!isMenuMobile) {
         mobileMenu.classList.add('inactive')
     }
+
 
     if (!isNavbarMenu) {
         navbarMenu.classList.add('inactive')
@@ -72,6 +101,8 @@ function loadProducts() {
         let imgProduct = document.createElement('img');
         imgProduct.setAttribute('src', product.image);
         imgProduct.setAttribute('alt', product.name);
+        imgProduct.setAttribute('data-id', product.id);
+        imgProduct.addEventListener('click', e => showDetailPorduct(e))
 
         let divInfo = document.createElement('div');
         let divProducInfo = document.createElement('div');
@@ -104,4 +135,28 @@ function loadProducts() {
         cardsContainer.appendChild(cardProduct)
 
     }
+}
+
+function showDetailPorduct(event) {
+
+    mobileMenu.classList.add('inactive')
+    imageArrow.classList.remove('traslate-arrow')
+
+    shoppingCart.classList.add('inactive')
+    shoppingCart.classList.remove('shopping-cart-display-grid')
+    cart.classList.add('inactive')
+
+    navbarMenu.classList.add('inactive')
+    productDetail.classList.remove('inactive')
+
+    getProduct(event.target.getAttribute('data-id'))
+}
+
+
+function getProduct(id) {
+
+    product = data.filter(d => d.id == id);
+    productDetailImage.setAttribute('src', product[0].image);
+    productDetailPrice.innerText = '$' + product[0].price;
+    productDetailName.innerText = product[0].name;
 }
