956a29a204a60545c0183d0cd10b746ffd701a2a
Jhonatan Padilla <jhoalparo1991@gmail.com>
2022-12-09 21:14:06 -0500

    integracion del carrito de compras en proceso

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 const toggleMenu = document.querySelector('#toggle-menu')
 const navbarMenu = document.querySelector('#navbar-menu')
 const imageArrow = document.querySelector('#image-arrow')
+const menuBarIcon = document.querySelector('.menu-bar')
+const mobileMenu = document.querySelector('.mobile-nav')
+const menuShoppingCart = document.querySelector('.figure')
+const shoppingCart = document.querySelector('.shopping-cart')
 
 toggleMenu.addEventListener('click', toggleMenuDesktop);
+menuBarIcon.addEventListener('click',toggleMenuBar);
+menuShoppingCart.addEventListener('click',toggleShoppingCart);
 
 function toggleMenuDesktop() {
 
     navbarMenu.classList.toggle('inactive')
     imageArrow.classList.toggle('traslate-arrow')
+}
+
+function toggleMenuBar() {
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCart() {
+
+    let isActive = shoppingCart.classList.toggle('inactive')
+    let isMenuMobile = mobileMenu.classList.contains('inactive')
+
+    if(isMenuMobile){
+        if(isActive){
+            shoppingCart.style.display='none';
+        }else{
+            
+            shoppingCart.style.display='grid';
+        }
+    }else{
+        shoppingCart.style.display='none';
+    }
+
+    
+
+    console.log(isMenuMobile);
 }
